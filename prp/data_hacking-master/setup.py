858f519c18cdc3b253a161ad18c91c55a88d6cf0
jeklen <zhql0907@outlook.com>
2017-01-13 12:14:18 +0800

    add prp

---
+++ prp/data_hacking-master/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+from data_hacking import __version__
+
+setup(name='data_hacking',
+      version=__version__,
+      author='Click',
+      author_email='clicklabs@clicksecurity.com',
+      description='Modules for Data Hacking project',
+      long_description=open('README.md').read(),
+      install_requires=[ 'networkx','pygraphviz','pandas','matplotlib','numpy','tldextract','sqlparse','macholib','pefile','patsy','statsmodels','sklearn' ],
+      url='http://clicksecurity.github.io/data_hacking',
+      packages=['data_hacking', 'data_hacking.min_hash','data_hacking.lsh_sims',
+                'data_hacking.hcluster','data_hacking.simple_stats','data_hacking.yara_signature'],
+      classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: BSD License",
+      ]
+     )
